54f305c2c7c8597cff641b39d72b1e201db54f95
unknown <raahmi02@gmail.com>
2025-06-15 03:21:40 +0700

    UAS

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
-// Middleware JSON
-app.use(express.json());
+// Middleware CORS (biar bisa fetch dari mana aja)
+app.use(cors());
 
-// CORS (biar bisa fetch dari mana aja)
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// Middleware JSON body parser
+app.use(express.json());
 
 // Serve file statis dari folder public
 app.use(express.static(path.join(__dirname, 'public')));
@@ -39,11 +36,12 @@ app.post('/submit', (req, res) => {
   res.status(200).send('Data berhasil disimpan');
 });
 
-// Mulai server
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running at http://0.0.0.0:${port}`);
+// Akses index.html dari root
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Jalankan server
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
